6bcd4e6924b29fb031b06503eac193d5dc43c0df
Hakan Berkiten <hakanberkiten7@gmail.com>
2025-02-15 21:34:59 +0300

    Redirect home page at the first place

--- e-commercial-app/src/app/app.routes.ts
+++ e-commercial-app/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SignUpComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignUpComponent },
