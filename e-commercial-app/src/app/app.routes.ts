823bb500267860a9fe53e9866935d33ee372e64e
abdullahsvnc <abdullahsvnc7@gmail.com>
2025-03-09 23:04:00 +0300

    home page icon and shopping page icon added and a shopping-cart component generated for showing cart and for store items in cart

--- e-commercial-app/src/app/app.routes.ts
+++ e-commercial-app/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes, provideRouter } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignUpComponent },
+    { path: 'shopping-cart', component: ShoppingCartComponent}
 ];
 
 export const appRouter = provideRouter(routes);
